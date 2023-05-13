fa8ca640582721536281e27896511ec036e1e72d
Rohit Kumar <thisisrohitkumar@outlook.com>
2023-05-13 21:36:32 +0530

    responsive hero and header

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 let timeout;
 
 function showPreloader() {
-  timeout = setTimeout(hidePreloader, 3000);
+  timeout = setTimeout(hidePreloader, 4000);
 }
 
 function hidePreloader() {
   document.getElementById('preloader-container').style.display = 'none';
+}
+
+function open_mobile_nav(){
+  document.getElementById('open-mobile-nav').style.display = 'none';
+  document.getElementById('close-mobile-nav').style.display = 'block';
+}
+
+function close_mobile_nav(){
+  document.getElementById('open-mobile-nav').style.display = 'block';
+  document.getElementById('close-mobile-nav').style.display = 'none';
 }
