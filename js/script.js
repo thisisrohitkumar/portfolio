8639897913bf8f16a30c9760aa0e4a8ffeb7738e
Rohit Kumar <thisisrohitkumar@outlook.com>
2023-05-13 22:24:56 +0530

    added responsiveness to about and contact section

--- js/script.js
+++ js/script.js
@@ -11,9 +11,11 @@ function hidePreloader() {
 function open_mobile_nav(){
   document.getElementById('open-mobile-nav').style.display = 'none';
   document.getElementById('close-mobile-nav').style.display = 'block';
+  document.getElementById('mobile-nav').style.transform = 'translateX(0)';
 }
 
 function close_mobile_nav(){
   document.getElementById('open-mobile-nav').style.display = 'block';
   document.getElementById('close-mobile-nav').style.display = 'none';
+  document.getElementById('mobile-nav').style.transform = 'translateX(100%)';
 }
